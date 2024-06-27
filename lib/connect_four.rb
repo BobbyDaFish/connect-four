2667162a683238be0d9765735e32845563333d1c
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-06-27 14:56:37 -0600

    Create starting directories and files for project

---
+++ lib/connect_four.rb
@@ -0,0 +1,14 @@
+# frozen-string-literal: true
+
+# class to contain game board, and verticies for piece graphs.
+class Game
+  def initialize
+    @board = [%w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _],
+              %w[_ _ _ _ _ _ _]]
+  end
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,7 @@
+# frozen-string-literal: true
+
+require '../lib/connect_four'
+
+describe Game do
+  subject(:game) { described_class.new }
+end
