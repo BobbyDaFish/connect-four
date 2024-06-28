32f7ae1e208d4014b27e7fe7a73d84a439c7cffa
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-06-28 13:09:36 -0600

    Update player class to correctly assign icons

--- lib/players.rb
+++ lib/players.rb
@@ -1,6 +1,11 @@
 # frozen-string-literal: true
 
 class Player
-  def initialize
+  def initialize(player_num)
+    @player_icon = if player_num == 1
+                     "\u25c6"
+                   else
+                     "\u25c9"
+                   end
   end
 end
