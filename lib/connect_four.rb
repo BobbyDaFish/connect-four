fcd3c4c59c43627916b8eeb8be1d73a1b40ed86b
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-06-28 13:08:29 -0600

    Create tests and methods for choosing players

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -12,10 +12,49 @@ class Game
               %w[_ _ _ _ _ _ _],
               %w[_ _ _ _ _ _ _],
               %w[_ _ _ _ _ _ _]]
-    @player1 = new_player
+    @player1 = new_player(1)
+    @player2 = new_player(2)
   end
 
-  def new_player
-    Player.new
+  def new_player(player_num)
+    Player.new(player_num)
+  end
+
+  def display_board
+    puts '1 2 3 4 5 6 7'
+    i = 0
+    until i == 7
+      puts @board[i].join(' | ')
+      i += 1
+    end
+  end
+
+  def valid_play?(play, board = @board)
+    return play if play.match?(/[1-7]/)
+    return play if board[0][play - 1] == '_'
+
+    false
+  end
+
+  def get_play(player)
+    puts 'Select a column between 1 and 7 that is not full to place your piece'
+    input = 0
+    loop do
+      input = gets.chomp.to_i
+      break if valid_play?(input)
+
+      puts 'Invalid selection. Select a column between 1 and 7 that is not full to place your piece.'
+    end
+    place_piece(input, player)
+  end
+
+  def place_piece(input, player, board = @board)
+    i = 1 # will always start with second row, as valid_input? checks if top row is full
+    loop do
+      break if board[i][input - 1] != '_'
+
+      i += 1
+    end
+    @board[i - 1][input - 1] = player.player_icon
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -11,12 +11,93 @@ describe Game do
 
   describe '#new_player' do
     before do
-      allow(Player).to receive(:new)
+      allow(game).to receive(:new_player).with(1).and_return("\u25c6")
     end
 
-    it 'sends initialize to Player class' do
-      expect(Player).to receive(:new)
-      game.new_player
+    it 'creates new player object' do
+      expect(game).to receive(:new_player).with(1)
+      game.new_player(1)
+    end
+  end
+
+  describe '#display_board' do
+    it 'prints all 7 rows and header' do
+      expect(game).to receive(:puts).exactly(8).times
+      game.display_board
+    end
+  end
+
+  describe '#valid_play?' do # rubocop:disable Metrics/BlockLength
+    context 'player submits valid choice' do
+      before do
+        valid_input = '3'
+        allow(game).to receive(:valid_play?).with(valid_input)
+      end
+
+      it 'receives valid input and returns that input' do
+        expect(game).to receive(:valid_play?).and_return(3)
+        game.valid_play?
+      end
+    end
+
+    context 'player submits number to full column' do
+      before do
+        full_column = '3'
+        board_full = [['_', '_', "\u25c6", '_', '_', '_', '_']]
+        allow(game).to receive(:valid_play?).with(full_column, board_full)
+      end
+
+      it 'returns false' do
+        expect(game).to receive(:valid_play?).and_return(false)
+        game.valid_play?
+      end
+    end
+
+    context 'player submits invalid input' do
+      before do
+        invalid_input = 'b'
+        allow(game).to receive(:valid_play?).with(invalid_input)
+      end
+
+      it 'returns false' do
+        expect(game).to receive(:valid_play?).and_return(false)
+        game.valid_play?
+      end
+    end
+  end
+
+  describe '#place_piece' do
+    context 'empty board, first play' do
+      before do
+        valid_play = 3
+        player = game.instance_variable_get(:@player1)
+        allow(game).to receive(:place_piece).with(valid_play, player)
+      end
+
+      it 'places piece in row 7' do
+        expect(game).to receive(:place_piece).and_return("\u25c6")
+        game.place_piece
+      end
+    end
+
+    context 'bottom row already has a piece' do
+      before do
+        play = 3
+        player = game.instance_variable_get(:@player2)
+        board = [%w[_ _ _ _ _ _ _],
+                 %w[_ _ _ _ _ _ _],
+                 %w[_ _ _ _ _ _ _],
+                 %w[_ _ _ _ _ _ _],
+                 %w[_ _ _ _ _ _ _],
+                 %w[_ _ _ _ _ _ _],
+                 ['_', '_', "\u25c6", '_', '_', '_', '_']]
+        allow(game).to receive(:place_piece).with(play, player, board)
+      end
+
+      it 'places the piece in row 6' do
+        expect(game).to receive(:place_piece).and_return("\u25c9")
+        game.place_piece
+      end
     end
   end
 end
